4a5f8d78be2467a08e5d0e373256820a79e532aa
eya <it.eya@gmail.com>
2018-03-26 00:25:22 -0600

    set False to debug

--- sistema/views.py
+++ sistema/views.py
@@ -204,7 +204,7 @@ def ReporteEmpresa(request):
 			Listado_Pagos = None
 			if op_mes.first() is not None:
 				#writer.writerow(['Nombre', 'Numero Expediente','No. Tenencias','Total a Pagar Q','Fecha Ingreso Oficina','Fecha Ingreso Digecam','Fecha Cita','Fecha Pago','Fecha Entrega','Quien Entrego','Quien Recibio','Estatus','cobros','Descripcion Estado'])
-				writer.writerow(['Numero Expediente','Nombre','Fecha Ingreso Oficina','Fecha Entrega','Estatus','No. Tenencias', 'No. Autentica Firma','cobros','Total a Pagar Q'])
+				writer.writerow(['Numero Expediente','Nombre','Fecha Ingreso Oficina','Fecha Entrega','Estatus','No. Tenencias', 'No. Autentica Firma','Total a Pagar Q','cobros'])
 				listado = Expediente.objects.all().filter(operacionmes__empresa__id=empresa,operacionmes__anio=fecha[0],operacionmes__mes=fecha[1])
 				listprecio = []
 				for ex in listado:
@@ -223,7 +223,7 @@ def ReporteEmpresa(request):
 							else:
 								precio += temp.first().precio
 					#writer.writerow([ex.cliente, ex.numeroexpe,ex.tenencias,precio,ex.fecha_ingreso_oficina,ex.fecha_ingreso_digecam,ex.fecha_cita,ex.fecha_pago,ex.fecha_entrega,ex.entrego,ex.recibio,ex.estatus,CobroDescripcion,ex.descripcion_estatus])
-					writer.writerow([ex.numeroexpe,ex.cliente,ex.fecha_ingreso_oficina,ex.fecha_entrega,ex.estatus,ex.tenencias,ex.autenticafirma,CobroDescripcion,precio])
+					writer.writerow([ex.numeroexpe,ex.cliente,ex.fecha_ingreso_oficina,ex.fecha_entrega,ex.estatus,ex.tenencias,ex.autenticafirma,precio,CobroDescripcion])
 					#Precio_Total += precio
 					#listprecio.append([ex,precio])
 					#Listado_Pagos = Abono.objects.filter(operacionmes__empresa__id=empresa,operacionmes__anio=fecha[0],operacionmes__mes=fecha[1])
